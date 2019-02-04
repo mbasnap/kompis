4f9088e24619239d71bf7da3039d0d6a3aa157db
mbaSnap <you@example.com>
2019-02-04 18:06:39 +0300

    zzz

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/posts', posts);
 // Server static assets if in production
 // if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/dist'));
+  app.use(express.static('client/dist/static'));
 
    app.get('*', (req, res) => {
      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
