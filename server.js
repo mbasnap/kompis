bb5125738961abd6cb811fd4204eea00ae2e7932
mbaSnap <you@example.com>
2019-02-04 18:26:55 +0300

    sss

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const serveStatic = require('serve-static');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -36,11 +37,12 @@ app.use('/api/posts', posts);
 // Server static assets if in production
 // if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/dist/static'));
+  // app.use(express.static('client/dist/static'));
+  app.use(serveStatic(__dirname + "/client/dist"));
 
-   app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-   });
+  //  app.get('*', (req, res) => {
+  //    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  //  });
 // }
 
 const port = process.env.PORT || 5000;
