47651f1e7c9ffcf055e7dd94dfe13b178e5775c8
mbaSnap <you@example.com>
2019-02-04 18:40:29 +0300

    Adding files.

--- server.js
+++ server.js
@@ -1,55 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
-const serveStatic = require('serve-static');
-
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
-const app = express();
-
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// DB Config
-const db = require('./config/keys').mongoURI;
-
-// Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-// Passport middleware
-app.use(passport.initialize());
-
-// Passport Config
-require('./config/passport')(passport);
-
-// Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-// Server static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  // app.use(express.static('client/dist/static'));
-  // app.use(serveStatic(__dirname + "/client/dist"));
-    app.use('/', (req, res) => {
-      return res.json({msg: 'use'});
-    })
-    app.get('*', (req, res) => {
-      return res.json({msg: 'det'});
-    });
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-    // });
-// }
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use('/', (req, res) => {
+  return res.json({msg: 'aDSAAS'})
+})
+// app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
