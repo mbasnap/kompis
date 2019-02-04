789286ebf256ff299e6032872a31a9dc51e8fadd
mbaSnap <you@example.com>
2019-02-04 19:06:55 +0300

    Adding files.

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// // DB Config
-// const db = require('./config/keys').mongoURI;
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-// // Connect to MongoDB
-// mongoose
-//   .connect(db)
-//   .then(() => console.log('MongoDB Connected'))
-//   .catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Passport middleware
 // app.use(passport.initialize());
