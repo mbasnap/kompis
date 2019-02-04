f5e91e4087370e1b64132880da9630c6f6772232
mbaSnap <you@example.com>
2019-02-04 17:50:25 +0300

    ddd

--- server.js
+++ server.js
@@ -34,14 +34,14 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/dist'));
 
    app.get('*', (req, res) => {
      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
    });
-}
+// }
 
 const port = process.env.PORT || 5000;
 
