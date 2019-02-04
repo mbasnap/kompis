cc3375648934a87df694781e76bc7c1e5dda4dfd
mbaSnap <you@example.com>
2019-02-04 19:04:04 +0300

    Adding files.

--- server.js
+++ server.js
@@ -11,28 +11,28 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-// DB Config
-const db = require('./config/keys').mongoURI;
+// // DB Config
+// const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// // Connect to MongoDB
+// mongoose
+//   .connect(db)
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
-// Passport Config
-require('./config/passport')(passport);
+// // Passport Config
+// require('./config/passport')(passport);
 
-// Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+// // Use Routes
+// app.use('/api/users', users);
+// app.use('/api/profile', profile);
+// app.use('/api/posts', posts);
 // Server static assets if in production
 // if (process.env.NODE_ENV === 'production') {
   // Set static folder
